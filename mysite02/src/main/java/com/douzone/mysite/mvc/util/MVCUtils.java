9939aface8038719fe3e49884e94a23e36735662
sinhu-jung <tlsgn26@naver.com>
2021-05-27 10:57:25 +0900

    mysite02 #1

---
+++ mysite02/src/main/java/com/douzone/mysite/mvc/util/MVCUtils.java
@@ -0,0 +1,18 @@
+package com.douzone.mysite.mvc.util;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MVCUtils {
+
+	public static void forward(String path, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/" + path + ".jsp");
+		rd.forward(request, response);
+		
+	}
+
+}
