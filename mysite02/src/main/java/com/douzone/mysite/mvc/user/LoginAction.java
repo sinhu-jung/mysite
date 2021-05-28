9f2b55bb121c4cb075446a4a6b98c55e73cee687
sinhu-jung <tlsgn26@naver.com>
2021-05-28 11:36:26 +0900

    cookies #02

--- mysite02/src/main/java/com/douzone/mysite/mvc/user/LoginAction.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/user/LoginAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.douzone.mvc.Action;
 import com.douzone.mvc.util.MVCUtils;
@@ -27,6 +28,9 @@ public class LoginAction implements Action {
 		}
 		
 		//인증처리(session 처리)
+		//true면 없으면 만들어 달라는 이야기 이고 false면 없으면 null을 달라는 이야기임
+		HttpSession session = request.getSession(true);
+		session.setAttribute("authUser", userVo);
 		
 		//main으로 redirect
 		MVCUtils.redirect(request.getContextPath(), request, response);
