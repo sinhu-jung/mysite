6810bdde8d9e1a89b9f091c6e17fa50595f44f2c
sinhu-jung <tlsgn26@naver.com>
2021-06-15 16:01:24 +0900

    ...

--- mysite04/src/main/java/com/douzone/mysite/vo/UserVo.java
+++ mysite04/src/main/java/com/douzone/mysite/vo/UserVo.java
@@ -1,8 +1,9 @@
 package com.douzone.mysite.vo;
 
-import org.hibernate.validator.constraints.Email;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
 
 public class UserVo {
 
