a98cb4a4e88add4875c614c4340ec946e348da02
sinhu-jung <tlsgn26@naver.com>
2021-06-15 12:30:48 +0900

    mysite04 #finish-webconfig

--- mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
+import com.douzone.config.web.FileuploadConfig;
 import com.douzone.config.web.MessageConfig;
 import com.douzone.config.web.MvcConfig;
+import com.douzone.config.web.SecurityConfig;
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({"com.douzone.mysite.controller", "com.douzone.mysite.exception"})
-@Import({MvcConfig.class, MessageConfig.class})
+@Import({MvcConfig.class, MessageConfig.class, FileuploadConfig.class, SecurityConfig.class})
 public class WebConfig {
 	
 }
